c9c97e7b6ea56fb9c58f6a353394bd9a51722508
Insomnia01001 <167763964+Insomnia01001@users.noreply.github.com>
2024-10-14 00:38:41 +0300

    Add files via upload

---
+++ 2.10 dr hw/main.py
@@ -0,0 +1,32 @@
+# son = 1
+# while son <=100:
+#     if son % 5!=0:
+#       print(son)
+#     son+=1
+
+
+# ismlar =[]
+# while 1 :
+#     inp1= input("ismingizn kirititng")
+#     if inp1 =="stop":
+#         break
+#     ismlar.append(inp1)
+# for i in ismlar:
+#     print(i,end=" ")
+
+
+
+# yigindi = 0
+
+# footbolchila = {}
+# while True:
+#     inp1  = input("footbolist ismini kiriting: ")
+#     if inp1 =="stop":
+#         break
+#     inp2  = int(input("footbolistni gollarini kiriting: "))
+#     footbolchila[inp1]=inp2
+# for i in footbolchila:
+#     print(f"ism {i}, gollari {footbolchila[i]}")
+# for yigindisi in footbolchila.values():
+#         yigindi+=yigindisi
+# print(f"javob {yigindi}")
